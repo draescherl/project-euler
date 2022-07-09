ca0bcbee62b9edde675ce29b189be571baea0ca1
Lucas DRAESCHER <lucas.draescher@gmail.com>
2022-07-10 00:16:21 +0200

    Added timing function and question 5.

--- scala/src/main/scala/ProjectEuler.scala
+++ scala/src/main/scala/ProjectEuler.scala
@@ -2,6 +2,15 @@ import scala.annotation.tailrec
 
 object ProjectEuler {
 
+  def time[R](block: => R): R = {
+    val t0 = System.currentTimeMillis()
+    val result = block    // call-by-name
+    val t1 = System.currentTimeMillis()
+    println("Elapsed time: " + (t1 - t0) + "ms.")
+    result
+  }
+
+
   def largestPrimeFactor(n: Long): Long = {
     @tailrec
     def factorize(n: Long, prime: Int = 2, list: List[Long] = Nil): List[Long] =
@@ -14,8 +23,22 @@ object ProjectEuler {
     factorize(n).max
   }
 
+  // Try using foldLeft by defining a LCM function (https://rosettacode.org/wiki/Least_common_multiple#Scala)
+  def smallestEvenlyDivisibleNumber(init: Int): Int = {
+    @tailrec
+    def divisible(n: Int, divider: Int): Int =
+      n % divider == 0 match {
+        case true if divider == 20 => n
+        case true                  => divisible(n, divider + 1)
+        case false                 => divisible(n + 1, 2)
+      }
+
+    divisible(init, 2)
+  }
+
   def main(args: Array[String]): Unit = {
-    println(largestPrimeFactor(600851475143L))
+    val result = time(smallestEvenlyDivisibleNumber(100))
+    println(result)
   }
 
 }
