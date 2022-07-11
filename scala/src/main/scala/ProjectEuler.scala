77e9325d9ad443bd7fa44e8df3f846f3a39bc5d4
Lucas DRAESCHER <lucas.draescher@gmail.com>
2022-07-11 14:22:56 +0200

    Solved problem 6.

--- scala/src/main/scala/ProjectEuler.scala
+++ scala/src/main/scala/ProjectEuler.scala
@@ -67,6 +67,18 @@ object ProjectEuler {
   val smallestEvenlyDivisibleNumberBis: Int = List.range(1, 20).foldLeft(1)(lcm)
 
 
+  /* -----------------------------------------------------------------------
+   Problem 6 : Find the difference between the sum of the squares of the
+   first one hundred natural numbers and the square of the sum.
+   ----------------------------------------------------------------------- */
+  val sums: Int = {
+    val n = 100
+    val sumSquares = Array.range(0, n + 1).map(x => x * x).sum
+    val squareSum = Math.pow(n * (n + 1) / 2, 2).toInt
+    squareSum - sumSquares
+  }
+
+
   // ------------------------------------------------------------------------------
   // ------------------------------------------------------------------------------
 
@@ -80,7 +92,7 @@ object ProjectEuler {
       result
     }
 
-    val result = time(largestPalindrome)
+    val result = time(sums)
     println(result)
   }
 
