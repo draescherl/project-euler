0d806782a7cb70c8029c593ba59f72ccf5443257
Lucas DRAESCHER <lucas.draescher@gmail.com>
2022-07-14 16:42:40 +0200

    Solved problem 25.

--- scala/src/main/scala/problems/Problem025.scala
+++ scala/src/main/scala/problems/Problem025.scala
@@ -1,15 +1,15 @@
 package problems
 
-import utils.Utils.{fibs, time}
+import utils.Utils.time
 
 /* -----------------------------------------------------------------------
  Problem 25 : What is the index of the first term in the Fibonacci
  sequence to contain 1000 digits?
  ----------------------------------------------------------------------- */
 object Problem025 {
-  // https://youtu.be/kWVPOpuAtzE
-  def fibonacciIndex(numOfDigits: Int): Option[Int] = {
-    fibs.find(_.toString.length == numOfDigits)
+  def fibonacciIndex(numOfDigits: Int): Int = {
+    lazy val bigFibs: LazyList[BigInt] = BigInt(0) #:: bigFibs.scanLeft(BigInt(1))(_ + _)
+    bigFibs.indexWhere(_.toString.length == numOfDigits)
   }
 
   def main(args: Array[String]): Unit =
