b7d42bd0b7ba91c6e2d65d99c56f55f7cf19557b
Lucas DRAESCHER <lucas.draescher@gmail.com>
2022-08-07 23:51:59 +0200

    Made object extend App

--- scala/src/main/scala/problems/Problem001.scala
+++ scala/src/main/scala/problems/Problem001.scala
@@ -4,9 +4,7 @@ import utils.Utils.time
 
 // https://projecteuler.net/problem=1
 // Sum of multiples of 3 or 5 under 1000
-object Problem001 {
-  def main(args: Array[String]): Unit = {
-    val result = (1 to 999).filter(x => (x % 3 == 0) || (x % 5 == 0)).sum
-    println(time(result))
-  }
+object Problem001 extends App {
+  val result = (1 to 999).filter(x => (x % 3 == 0) || (x % 5 == 0)).sum
+  println(time(result))
 }
